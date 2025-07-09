b133f7a6d3a8b00e6249f011a718f4d88a43299b
Idumii <vincent.dievart.8@gmail.com>
2025-07-09 14:53:12 +0200

    modif chemin fichier

---
+++ BDD/import_game_platform.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+# Chargement des variables d'environnement depuis le fichier .env
+load_dotenv()
+# Récupération des informations de connexion à la base de données
+db_password = os.getenv('BDD_PSWD')
+
+# Connexion à ta base de données chez o2switch
+conn = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3307,
+    user='bawi2179_idumi',
+    password=db_password,
+    database='bawi2179_data_game',
+    allow_local_infile=True
+)
+
+cursor = conn.cursor()
+
+# Chemin vers ton fichier CSV local
+csv_path = r"C:\Users\User\Documents\Data-Game\data-game\csv\game_platform_exploded.csv"
+
+query = f"""
+LOAD DATA LOCAL INFILE '{csv_path.replace("\\", "/")}'
+INTO TABLE game_platform
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\\n'
+IGNORE 1 LINES
+(game_id, platform_id);
+"""
+
+try:
+    cursor.execute(query)
+    conn.commit()
+    print(f"✅ Import terminé avec succès.")
+except Exception as e:
+    print("❌ Erreur lors de l'import :", e)
+
+cursor.close()
+conn.close()
