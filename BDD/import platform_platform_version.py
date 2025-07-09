5fb293ce4111193bbf6fee1328fd3a2cd0b48e18
iampdv <martin.potdevin@gmail.com>
2025-07-09 14:52:34 +0200

    ajout platform / platform-version / game_multi

---
+++ BDD/import platform_platform_version.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+# Connexion à ta base de données chez o2switch
+conn = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3307,
+    user='bawi2179_idumi',
+    password='zyLeM]F_lkz$',
+    database='bawi2179_data_game',
+    allow_local_infile=True
+)
+
+cursor = conn.cursor()
+
+# Chemin vers ton fichier CSV local
+csv_path = r"C:\Users\User\Documents\Data-Game\data-game\csv\platform_platform_version.csv"
+
+query = f"""
+LOAD DATA LOCAL INFILE '{csv_path.replace("\\", "/")}'
+INTO TABLE platform_platform_version
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\\n'
+IGNORE 1 LINES
+(platform_id, version_id);
+"""
+
+try:
+    cursor.execute(query)
+    conn.commit()
+    print(f"✅ Import terminé avec succès.")
+except Exception as e:
+    print("❌ Erreur lors de l'import :", e)
+
+cursor.close()
+conn.close()
