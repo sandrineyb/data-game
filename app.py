5fd7bb861e90b51457445276a73b48bcab424a5a
Idumii <vincent.dievart.8@gmail.com>
2025-07-15 14:41:54 +0200

    Modif structure app

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')  # Assure-toi d'avoir un fichier templates/index.html
 
+if __name__ == "__main__":
+    app.run(debug=True)
