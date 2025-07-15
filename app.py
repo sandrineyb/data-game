193971ed994991ad2cc59eb522ae38a274d8bcfd
Idumii <vincent.dievart.8@gmail.com>
2025-07-15 16:08:05 +0200

    Base Navbar footer bootstrap / route de bases de navigation

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')  # Assure-toi d'avoir un fichier templates/index.html
 
+@app.route('/jeux')
+def jeux():
+    return render_template('jeu.html')
+
+@app.route('/consoles')
+def consoles():
+    return render_template('consolex.html')
+
+@app.route('/entreprises')
+def entreprises():
+    return render_template('entreprises.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
